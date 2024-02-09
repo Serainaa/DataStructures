10d1219bad5ebde1bc3fa6d1eb03ed84ec2e47e8
Serainaa <seraina7@yahoo.gr>
2024-02-09 22:02:53 +0200

    Add comments to stack implementations

--- Stack-Array.js
+++ Stack-Array.js
@@ -1,26 +1,33 @@
+// Define a class Stack to implement the stack data structure.
 class Stack {
     constructor() {
-      this.array = [];
+      this.array = []; // Initialize an array to store stack elements
     }
 
+    // Method to view the top element of the stack without removing it.
     peek() {
-      console.log(this.array[this.array.length-1]);
+      console.log(this.array[this.array.length-1]); // Log the top element of the stack
     }
 
+    // Method to add a new element to the top of the stack.
     push(value) {
-      this.array.push(value)
+      this.array.push(value); // Add the value to the end of the array
     }
     
+    // Method to remove the top element from the stack.
     pop() {
-      this.array.pop()
+      this.array.pop(); // Remove the last element from the array
     } 
   }
   
-  const myStack = new Stack();
-  console.log(myStack.push("google"));
-  console.log(myStack.push("youtube"));
-  console.log(myStack.push("wiki"));
-  console.log(myStack.peek());
-  console.log(myStack.pop());
-  console.log(myStack.peek());
-  console.log(myStack.pop());
+// Create an instance of the Stack class.
+const myStack = new Stack();
+
+// Test the stack methods.
+console.log(myStack.push("google"));  // Push "google" onto the stack
+console.log(myStack.push("youtube")); // Push "youtube" onto the stack
+console.log(myStack.push("wiki"));    // Push "wiki" onto the stack
+console.log(myStack.peek());          // View the top element of the stack
+console.log(myStack.pop());            // Pop the top element from the stack
+console.log(myStack.peek());           // View the top element of the stack
+console.log(myStack.pop());            // Pop the top element from the stack
--- Stack-LinkList.js
+++ Stack-LinkList.js
@@ -1,57 +1,67 @@
+// Define a class Node to represent individual elements in the stack.
 class Node {
     constructor(value) {
-      this.value = value;
-      this.next = null;
+      this.value = value; // Value of the node
+      this.next = null;   // Pointer to the next node
     }
   }
   
+// Define a class Stack to implement the stack data structure.
 class Stack {
     constructor() {
-      this.top = null;
-      this.bottom = null;
-      this.length = 0;
+      this.top = null;    // Reference to the top element of the stack
+      this.bottom = null; // Reference to the bottom element of the stack
+      this.length = 0;    // Number of elements in the stack
     }
 
+    // Method to view the top element of the stack without removing it.
     peek() {
-      return console.log(this.top);
+      return console.log(this.top); // Log the top element
     }
 
+    // Method to add a new element to the top of the stack.
     push(value) {
-      let new_node = new Node(value);
+      let new_node = new Node(value); // Create a new node with the given value
       if (this.isEmpty()) {
-        this.bottom = new_node;
-        this.top = new_node;
+        this.bottom = new_node; // If stack is empty, set the bottom to the new node
+        this.top = new_node;    // Set the top to the new node
       } else {
-        new_node.next = this.top; 
-        this.top = new_node;
+        new_node.next = this.top; // Set the next of the new node to the current top
+        this.top = new_node;      // Update the top to the new node
       }
-      this.length++;
-      return this;
+      this.length++; // Increment the length of the stack
+      return this;   // Return the updated stack
     }
     
+    // Method to remove the top element from the stack.
     pop() {
       if (this.isEmpty())
-        return null;
-      else
+        return null; // If stack is empty, return null
+      else {
         if (this.top === this.bottom)  
-          this.bottom = null; 
-        this.top = this.top.next;
-        this.length--;
-        return this;
+          this.bottom = null; // If only one element, set bottom to null
+        this.top = this.top.next; // Update the top to the next element
+        this.length--; // Decrement the length of the stack
+        return this; // Return the updated stack
+      }
     }
     
+    // Method to check if the stack is empty.
     isEmpty() {
       if (this.bottom === null)
-        return true
+        return true; // If bottom is null, stack is empty
       else
-        return false
+        return false; // Otherwise, stack is not empty
     }
   }
   
-  const myStack = new Stack();
-  console.log(myStack.push("google"));
-  console.log(myStack.push("youtube"));
-  console.log(myStack.push("wiki"));
-  console.log(myStack.peek());
-  console.log(myStack.pop());
-  console.log(myStack.pop());
+// Create an instance of the Stack class.
+const myStack = new Stack();
+
+// Test the stack methods.
+console.log(myStack.push("google"));  // Push "google" onto the stack
+console.log(myStack.push("youtube")); // Push "youtube" onto the stack
+console.log(myStack.push("wiki"));    // Push "wiki" onto the stack
+console.log(myStack.peek());          // View the top element of the stack
+console.log(myStack.pop());            // Pop the top element from the stack
+console.log(myStack.pop());            // Pop the top element from the stack
